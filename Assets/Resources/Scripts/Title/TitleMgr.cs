e9cc8f4efaf953591d94d2bcd2440f8d67f427c0
MYCOMPUTER\山城主水 <mondo-y-17e@stu.kbc.ac.jp>
2019-09-26 13:50:26 +0900

    <追加>　コメントを追加

--- Assets/Resources/Scripts/Title/TitleMgr.cs
+++ Assets/Resources/Scripts/Title/TitleMgr.cs
@@ -16,6 +16,7 @@ public class TitleMgr : MonoBehaviour
 
     void Update()
     {
+        // Bボタンか左クリックで次のシーンへ
         if (Input.GetButtonDown("PAD_B_BUTTON") || Input.GetMouseButtonDown(0))
         {
             SceneManager.LoadScene("Main");
